adc5ad23cac3272e4030ee8d16a561b2d9e35684
Neha kumari <kumarineha122162@gmail.com>
2024-12-04 20:34:43 +0530

    Using redux

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+const GOOGLE_API_KEY = "AIzaSyDIDx5wLdWHCI8Y7x_DTVhkQZSteQgls1k"
+
+// const GOOGLE_API_KEY = "AIzaSyCuU0ewETLJvrGqrEEz_vXhvPNrWRGgSlk";
+
+export const YOUTUBE_VIDEOS_API ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
+  GOOGLE_API_KEY;
